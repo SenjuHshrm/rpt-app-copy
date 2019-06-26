58ccb2253c56d72c495d5aa60e64dcdae3e45a5c
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-06-26 16:56:44 +0800

    WIP: Segregation basic interface

--- src/app/segregation/segregation.component.ts
+++ src/app/segregation/segregation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { selectOpt } from '../interfaces/selectOpt';
 
 @Component({
   selector: 'app-segregation',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SegregationComponent implements OnInit {
 
+  param: string = 'pin';
+  req: string;
+
+  params: selectOpt[] = [
+    { value: 'pin', viewVal: 'PIN' },
+    { value: 'arpNo', viewVal: 'ARP No.' },
+    { value: 'name', viewVal: 'Name' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    if(!localStorage.getItem('auth')) {
+      window.location.href ='/';
+    }
+  }
+
+  search() {
+
   }
 
 }
